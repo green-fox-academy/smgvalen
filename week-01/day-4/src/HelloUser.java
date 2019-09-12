6f11323e2d362bde51f0a127580d9094035fc980
smgvalen <valentin.sumegi@gmail.com>
2019-09-12 16:12:51 +0200

    Started use Scanner

--- week-01/day-4/src/HelloUser.java
+++ week-01/day-4/src/HelloUser.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class HelloUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        // Modify this program to greet user instead of the World!
+        // The program should ask for the name of the user
+
+        System.out.println("Hello User, write here your name!");
+
+        String HelloUser = scanner.nextLine();
+
+        System.out.println("Hello " + HelloUser);
+
+    }
 }
