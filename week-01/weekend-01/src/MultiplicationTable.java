fd18c47e409af36719dc8f2c15cfef3bb8311d0a
smgvalen <valentin.sumegi@gmail.com>
2019-09-14 16:39:48 +0200

    First tests with for loops

---
+++ week-01/weekend-01/src/MultiplicationTable.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Create a program that asks for a number and prints the multiplication table with that number
+//
+// Example:
+// The number 15 should print:
+
+        System.out.println("Give me a number:");  //print this line to the terminal
+        Scanner scanner = new Scanner(System.in);  // create scanner to input something in the terminal
+        int i = scanner.nextInt();  // program can scan an integer
+        for ( int j = 0; j < 10 ; j++) {        // 
+            System.out.println(j + 1 + " * " + i + " = " + i*j);
+        }
+
+    }
+}
---
+++ week-01/weekend-01/src/PrintEven.java
@@ -0,0 +1,12 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        // Create a program that prints all the even numbers between 0 and 500
+        // 1. create and initialize the first variable of the statement
+        // 2. till when repeats
+        // 3. every round modify the value of the variable
+        for (int i = 2; i < 501; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+}
