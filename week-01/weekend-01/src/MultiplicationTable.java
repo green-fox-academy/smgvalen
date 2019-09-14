155cb24fbb4bd0cde4a2f2ce904a569caa6d9b02
smgvalen <valentin.sumegi@gmail.com>
2019-09-14 17:56:52 +0200

    For loops excercises

--- week-01/weekend-01/src/MultiplicationTable.java
+++ week-01/weekend-01/src/MultiplicationTable.java
@@ -10,7 +10,7 @@ public class MultiplicationTable {
         System.out.println("Give me a number:");  //print this line to the terminal
         Scanner scanner = new Scanner(System.in);  // create scanner to input something in the terminal
         int i = scanner.nextInt();  // program can scan an integer
-        for ( int j = 0; j < 10 ; j++) {        // 
+        for ( int j = 0; j < 10 ; j++) {        //
             System.out.println(j + 1 + " * " + i + " = " + i*j);
         }
 
