a60717f27759135812801f6abc3cd4d66d2f59ed
smgvalen <valentin.sumegi@gmail.com>
2019-09-23 22:44:11 +0200

    drawing diamond shape with stars done

--- week-01/weekend-01/src/DrawDiamond.java
+++ week-01/weekend-01/src/DrawDiamond.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
 public class DrawDiamond {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Lines of diamond: ");
-        int n = scanner.nextInt();
 
-        for (int i = 0; i <= n; i++) {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Lines of diamond: ");
+    int n = scanner.nextInt();
 
-            for (int j = n; j > i; j--) {
-                System.out.print("0");
-
-                for (int k = 0; k < j; k++) {
-                    System.out.print(" *");
-                }
-                System.out.println();
-            }
-
-        //    for (int l = n - 1; l >; l--)
-
-        }
-
-
+    for (int i = 0; i <= n / 2; i++) {
+      for (int j = n; j > i; j--) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < i; k++) {
+        System.out.print(" *");
+      }
+      System.out.println();
     }
+
+    for (int i = n / 2 + 1; i >= 0; i--) {
+      for (int j = n; j > i; j--) {
+        System.out.print(" ");
+      }
+      for (int k = 0; k < i; k++) {
+        System.out.print(" *");
+      }
+      System.out.println();
+    }
+  }
 }
+
