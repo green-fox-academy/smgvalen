14f69aa1f8d97a3bf0d9a973eb9153732e753563
smgvalen <valentin.sumegi@gmail.com>
2019-09-25 14:37:27 +0200

    objects and constructors practice

---
+++ week-03/day-03/src/blogpost/BlogPost.java
@@ -0,0 +1,21 @@
+package blogpost;
+
+public class BlogPost {
+
+  String authorName;
+  String title;
+  String text;
+  String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+}
+/* Create a BlogPost class that has
+an authorName
+a title
+a text
+a publicationDate  */
