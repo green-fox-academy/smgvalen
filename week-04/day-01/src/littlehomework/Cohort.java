ab2797dd13da6a58f27a553d67f669056734dc28
Valentin <valentin.sumegi@gmail.com>
2019-10-01 21:07:17 +0200

    homework before subject inheritance

--- week-04/day-01/src/littlehomework/Cohort.java
+++ week-04/day-01/src/littlehomework/Cohort.java
@@ -1,4 +1,17 @@
 package littlehomework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cohort {
+    String name;
+    ArrayList<Student> students;
+    ArrayList<Mentor> mentors;
+
+
+
+    public void addStudent (Student student) {
+        students.add(student);
+
+    }
 }
