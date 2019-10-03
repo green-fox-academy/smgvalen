9bf4d4c6e11b8e544c454ec4ae25013f101df740
Valentin <valentin.sumegi@gmail.com>
2019-10-03 22:51:28 +0200

    testing task about counting letters in a word. unfinished

--- week-04/day-03/src/countletters/CountLetters.java
+++ week-04/day-03/src/countletters/CountLetters.java
@@ -1,5 +1,21 @@
 package countletters;
 
+import java.util.HashMap;
+
 public class CountLetters {
 
+  public HashMap createDictionary(String word) {
+    char[] charArray = word.toCharArray();
+    HashMap<Character, Integer> charCounter = new HashMap<>();
+    for ( char i : charArray ) {
+     // charCounter.containsKey(charsOfWord[i]);
+      if ( charCounter.get(i) != charCounter.get(i+1) ) {
+        charArray.equals(createDictionary(word));
+      }
+    }
+    return charCounter;
+  }
+
 }
+//Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+//Create a test for that.
--- week-04/day-03/test/countletters/CountLettersTest.java
+++ week-04/day-03/test/countletters/CountLettersTest.java
@@ -1,5 +1,22 @@
+package countletters;
+
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import org.junit.Test;
 
 public class CountLettersTest {
 
+  @Test
+  public void createDictionary_SetCharsIntoHM() {
+    CountLetters testCountLetters = new CountLetters();
+    HashMap<Character, Integer> resultHashMap = new HashMap<>();
+
+    resultHashMap.put('m', 3);
+    resultHashMap.put('a', 1);
+    resultHashMap.put('u', 1);
+    resultHashMap.put('t', 1);
+
+    assertEquals("mammut", testCountLetters.createDictionary("mammut"));
+  }
 }
