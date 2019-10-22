6f3f27c4169ef1966e5b810bbcee9ba88819de9a
smgvalen <valentin.sumegi@gmail.com>
2019-10-22 11:04:57 +0200

    used stream to count the average of odd numbers of a list

---
+++ week-06/day-01/src/Exercise4.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise4 {
+
+  public static void main(String[] args) {
+
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+
+    double averageOfOdds = numbers.stream().filter(i -> i % 2 != 0)
+        .mapToInt((x) -> x).summaryStatistics().getAverage();
+
+    System.out.println("average of all odd numbers: " + averageOfOdds);
+  }
+}
