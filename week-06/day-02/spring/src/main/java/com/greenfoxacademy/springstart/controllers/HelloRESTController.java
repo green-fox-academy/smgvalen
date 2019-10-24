9d8f904372844baad5aa6d4f7495c70541aa4d4d
smgvalen <valentin.sumegi@gmail.com>
2019-10-24 17:10:33 +0200

    localhost greets user, counts how many times called

--- week-06/day-02/spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-06/day-02/spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,15 +1,21 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.controllers.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
+  AtomicLong atomicLong = new AtomicLong();
 
   @RequestMapping(value = "/greeting")
-  public Greeting greeting() {
-    Greeting greets = new Greeting(1, "Hello, World!");
+  public Greeting greeting(@RequestParam String name) {
+
+    Greeting greets = new Greeting(atomicLong.getAndIncrement()+1, name);
+
+
     return greets;
   }
 }
