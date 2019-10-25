5ca26f324385bf136b23fe26e9d3e71d1ca5230f
smgvalen <valentin.sumegi@gmail.com>
2019-10-25 11:58:11 +0200

    working endpoints, greeting diff languages

--- week-06/day-02/spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-06/day-02/spring/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,27 +1,49 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
-  AtomicLong counter = new AtomicLong();
 
-  @RequestMapping(value="/web/greeting")  // http://localhost:8080/web/greeting
+  AtomicLong counter = new AtomicLong();
+  String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj",
+      "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló",
+      "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry",
+      "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem",
+      "Sawubona"};
+  Random random = new Random();
+
+  @RequestMapping(value = "/web/greeting")  // http://localhost:8080/web/greeting
   public String greeting(Model model) {
     model.addAttribute("name", " World");
     return "greeting";
   }
 
-
-  @RequestMapping(value="/web")  // http://localhost:8080/web/?name=valen
-  public String countAndGreetByName (Model model, @RequestParam String name) {
+  @RequestMapping(value = "/web")  // http://localhost:8080/web/?name=valen
+  public String countAndGreetByName(Model model, @RequestParam String name) {
     model.addAttribute("name", name);
     model.addAttribute("counter", counter.incrementAndGet());
 
     return "greetingCounterByName";
+  }
+
+  @GetMapping(value = "web/sayHelloToAll")
+  // http://localhost:8080/web/sayHelloToAll?color=red&fontSize=74
+  public String sayGreetingAllLanguages(Model model, @RequestParam(required = false) String color,
+      @RequestParam(required = false) String fontSize) {
+    model.addAttribute("color", color);
+    model.addAttribute("fontSize", Integer.parseInt(fontSize));
+    model.addAttribute("text", hellos[random.nextInt(hellos.length)]);
+
+    return "sayHelloToAll";
   }
 }
