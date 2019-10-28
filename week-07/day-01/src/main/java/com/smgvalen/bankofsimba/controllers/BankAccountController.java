f7a3773f4230789ecfa6ecedc82c678ecf91474b
smgvalen <valentin.sumegi@gmail.com>
2019-10-28 17:58:06 +0100

    simba's bank created, existing methods worked, display worked

--- week-07/day-01/src/main/java/com/smgvalen/bankofsimba/controllers/BankAccountController.java
+++ week-07/day-01/src/main/java/com/smgvalen/bankofsimba/controllers/BankAccountController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BankAccountController {
@@ -13,7 +15,12 @@ public class BankAccountController {
   private List<BankAccount> bankAccountList = new ArrayList<>();
 
   public BankAccountController() {
-    bankAccountList.add(new BankAccount("Simba", 2000, "lion"));
+    bankAccountList.add(new BankAccount("Simba", 2000, "lion", true, true));
+    bankAccountList.add(new BankAccount("Zordon", 1700, "lion", false));
+    bankAccountList.add(new BankAccount("Nala", 1000, "lion", true));
+    bankAccountList.add(new BankAccount("Timon", 700, "meerkat", true));
+    bankAccountList.add(new BankAccount("Rafiki", 500, "baboon", true));
+    bankAccountList.add(new BankAccount("Zazu", 120, "hornbill", true));
   }
 
   @GetMapping(value = "/show")
@@ -28,9 +35,14 @@ public class BankAccountController {
     return "text";
   }
 
-
-
-  @GetMapping(value="/showAccounts")
-
+  @PostMapping(value ="/raise")
+  public String raise(Model model, @RequestParam int animalIndex){
+    if(bankAccountList.get(animalIndex).isKing()){
+      bankAccountList.get(animalIndex).setBalance(bankAccountList.get(animalIndex).getBalance() + 100);
+    }else{
+      bankAccountList.get(animalIndex).setBalance(bankAccountList.get(animalIndex).getBalance() + 10);
+    }
+    return "redirect:/show";
+  }
 
 }
--- week-07/day-01/src/main/java/com/smgvalen/bankofsimba/models/BankAccount.java
+++ week-07/day-01/src/main/java/com/smgvalen/bankofsimba/models/BankAccount.java
@@ -5,11 +5,23 @@ public class BankAccount {
   private String name;
   private Integer balance;
   private String animalType;
+  private boolean King;
+  private boolean goodGuy;
 
-  public BankAccount(String name, Integer balance, String animalType) {
+  public BankAccount(String name, Integer balance, String animalType, boolean goodGuy) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+    this.King = false;
+    this.goodGuy = goodGuy;
+  }
+
+  public BankAccount(String name, Integer balance, String animalType, boolean goodGuy, boolean King) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+    this.goodGuy = goodGuy;
+    this.King = King;
   }
 
   public String getName() {
@@ -34,5 +46,21 @@ public class BankAccount {
 
   public void setAnimalType(String animalType) {
     this.animalType = animalType;
+  }
+
+  public boolean isKing() {
+    return King;
+  }
+
+  public void setKing(boolean king) {
+    King = king;
+  }
+
+  public boolean isGoodGuy() {
+    return goodGuy;
+  }
+
+  public void setGoodGuy(boolean goodGuy) {
+    this.goodGuy = goodGuy;
   }
 }
